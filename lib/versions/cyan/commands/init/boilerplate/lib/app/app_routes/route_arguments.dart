289cba48fb69a29b06c81ef70ccaf03ac350e4f1
VikasPatel <vikasp@solguruz.com>
2025-09-26 21:01:38 +0530

    fix: some minor fixes on start

--- lib/versions/cyan/commands/init/boilerplate/lib/app/app_routes/route_arguments.dart
+++ lib/versions/cyan/commands/init/boilerplate/lib/app/app_routes/route_arguments.dart
@@ -1,29 +1,4 @@
 /// Will be passed to arguments (Map) while navigating to a screen/bottom sheet/dialog
 class RouteArguments {
   const RouteArguments._();
-
-  static const successTitle = 'success_title';
-  static const successMessage = 'success_message';
-  static const email = 'email';
-  static const firstName = 'first_name';
-  static const lastName = 'last_name';
-  static const code = 'code';
-  static const selectedCountry = 'selected_country';
-  static const projectFilter = 'project_filter';
-  static const projectOption = 'project_option';
-  static const project = 'project';
-  static const password = 'password';
-  static const selectedDate = 'selected_date';
-  static const projectId = 'project_id';
-  static const testTitle = 'test_title';
-  static const testItem = 'test_item';
-  static const foundationType = 'foundation_type';
-  static const isEditMode = 'is_edit_mode';
-  static const options = 'multiple_options';
-  static const cloudFile = 'cloudImage';
-  static const imageFile = 'imageFile';
-  static const materials = 'materials';
-  static const callback = 'callback';
-  static const progress = 'progress';
-  static const date = 'date';
 }
--- lib/versions/cyan/commands/init/boilerplate/lib/core/shared_pref/shared_pref.dart
+++ lib/versions/cyan/commands/init/boilerplate/lib/core/shared_pref/shared_pref.dart
@@ -6,9 +6,9 @@ part 'pref_manager.dart';
 class Prefs {
   Prefs._();
 
-  static late _PrefManager _prefManager;
+  static final _PrefManager _prefManager = _PrefManager();
 
-  static Future<void> initialize() async => _prefManager.initialize();
+  static Future<void> initialize() async => await _prefManager.initialize();
 
   static Future<void> clear() async => _prefManager.clear();
 }
