8203f5a089b48bf5361e953acb1383eacf079b67
VikasPatel <vikasp@solguruz.com>
2025-09-29 12:11:17 +0530

    ref: removed example as its no longer needed

--- example/lib/app_routes/route_names.dart
+++
@@ -1,5 +0,0 @@
-class Routes {
-  Routes._();
-  static const String initialRoute = signIn;
-  static const String signIn = '/sign_in';
-}
--- example/lib/pages/sign_in/bloc/sign_in_event.dart
+++
@@ -1,8 +0,0 @@
-part of 'sign_in_bloc.dart';
-
-abstract class SignInEvent with EquatableMixin implements AnalyticsEvent {
-  const SignInEvent();
-
-  @override
-  bool shouldLogEvent() => true;
-}
